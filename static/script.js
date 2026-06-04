1dc8f9dc29407153c021226af8d1f2db65a3202f
Kamilah McFarlane <kamilahzakmac@gmail.com>
2026-06-04 14:16:39 -0500

    update

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
       // ── Config ────────────────────────────────────────────────────────────
         const SERVER_IP   = "10.22.36.22";
         //const SERVER_IP   = "192.168.100.65";
-        
+        //tets 
         const SERVER_PORT = "8000";
         const API_BASE    = `http://${SERVER_IP}:${SERVER_PORT}`;
         const WS_URL      = `ws://${SERVER_IP}:${SERVER_PORT}/ws`;
